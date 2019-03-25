16791ca2fea1accb638bc27a89d369264c722fea
Ildar Zalyalov <ildarzalyalovmos@gmail.com>
2019-03-25 14:41:54 +0300

    Initial Commit

---
+++ UnitTests/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UnitTests
+//
+//  Created by Ильдар Залялов on 25/03/2019.
+//  Copyright © 2019 Ильдар Залялов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
